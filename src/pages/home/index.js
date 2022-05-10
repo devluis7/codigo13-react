7090153df624479beed5548c4c1b3ca91d81e072
Luis Enrique Bazan Rojas <luis.bazan@grupomok.com>
2022-05-10 01:14:30 -0500

    ultima act 09-05

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,15 @@
 // En material existe un tag llamado container
 import { useEffect, useState} from "react";
-import { Container } from "@mui/material";
+import { Container, Card, CardContent, CardMedia } from "@mui/material";
 import { getDataFromPokemon } from "../../service";
 // vamos a ver como podemos ejecutar la función que se encarga de traer a los
 // pokemones
 
 const Home = () => {
+    const imgUrl =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
+
+
     // vamos a crear una variable la cual se encarga de guardar la lista de pokemones
 
     const [pokemons, setPokemon] = useState([]);
@@ -31,14 +35,43 @@ const Home = () => {
         //! esto se hace para evitar un bucle infinito, si no colocamos el array vacio
         //! la función que esta dentro del useEffect se llamada n veces en forma infinita
 
-    },[]);
+    }, []);
 
     return (
         <Container>
-            <h1>
-                Pokedex
-            </h1>
-        </Container>
+            <h1>Pokedex</h1>
+            {/* es bueno validar que el array este lleno para que recien ejecute el map */}
+            {/* && estamos simulando un if  */}
+            {/* if (pokemons.lenght > 0) {
+                "lleno"
+            } */}
+            {/* pokemons.lenght > 0 && "leno" */}
+            {/* pokemons.lenght > 0 ? "lleno" : "vacio" */}
+    
+            {/*  Operador ternario => if {} else {}  */}
+            {/* {pokemons.length > 0 ? (
+            pokemons.map((pokemon) => (
+                // aca el codigo visual
+                <p>{pokemon.name}</p>
+            ))
+            ) : (
+            <h4>No hay datos</h4>
+            )} */}
+  
+            {pokemons.length > 0 &&
+                pokemons.map((pokemon,index) => (
+                // aca el codigo visual
+               <Card sx={{ width:300 }}>
+                   <CardMedia 
+                    component="img" 
+                    image={`${imgUrl}${index + 1}.svg`} 
+                />
+                   <CardContent>
+                       <h3>{pokemon.name}</h3>
+                   </CardContent>
+               </Card>
+            ))}
+      </Container>
     );
 };
 
